1e5e14201d63a53e312166777a7db5f9d8c25272
Roman Eckert <mail@romaneckert.de>
2017-10-12 21:42:57 +0200

    update book repository

--- repository/book.js
+++ repository/book.js
@@ -3,7 +3,7 @@ const AbstractRepository = require('@jeneric/core/abstract-repository');
 class Book extends AbstractRepository {
 
     constructor() {
-        super('book');
+        super();
     }
 }
 
