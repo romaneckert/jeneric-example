3606ab2b597c996f7791554802e63de0ce92a5e8
Roman Eckert <mail@romaneckert.de>
2017-10-21 19:24:29 +0200

    add system check

--- index.js
+++ index.js
@@ -7,11 +7,58 @@ class Main extends AbstractApplication {
 
         super(config);
 
-        let book = new this.models.book('Moby Dick');
-
-        this.logger.debug('new book with title ' + book.title + ' created', book);
+        this._initialCheck = false;
+        this._loopInterval = setInterval(this.loop.bind(this), 8);
 
     }
+
+    loop() {
+
+        if (false === this.systemCheck) {
+
+            // check system, if initial check correct but system check not correct stop running update method
+            if (true === this._initialCheck) {
+                clearInterval(this._loopInterval);
+                throw new Error('system check false but initial check true.');
+            }
+
+            return false;
+
+        }
+
+        if(!this._initialCheck) {
+            this._start();
+        }
+
+        this._initialCheck = true;
+
+
+    }
+
+    _start() {
+        let book = new this.models.book('Moby Dick');
+
+        this.data.add(book);
+
+        this.repositories.log.find({}, this._handleGetLogs.bind(this));
+    }
+
+    _handleGetLogs(logs) {
+        console.log(logs);
+    }
+
+    get systemCheck() {
+
+        // check if data service is ready
+        if(!this.services.data.ready) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+
 }
 
 let main = new Main(config);
